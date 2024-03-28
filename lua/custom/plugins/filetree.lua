6f686d3e1cdeab3e0e833d943716d7169c6fb3a2
Sam Huckaby <samhuckaby@gmail.com>
2024-03-28 08:56:58 -0400

    Add a filetree for when I'm lazy

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,13 @@
+-- Include an optional file tree for when I'm lazy (don't tell Teej)
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {}
+  end,
+}
