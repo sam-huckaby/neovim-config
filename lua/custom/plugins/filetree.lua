7d5b4127f6bd47710442423a083689f50c2a4143
Sam Huckaby <samhuckaby@gmail.com>
2024-04-22 14:57:46 -0400

    Fix Neotree's follow_current_file configuration

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -10,7 +10,9 @@ return {
   config = function()
     require('neo-tree').setup {
       filesystem = {
-        follow_current_file = true,
+        follow_current_file = {
+          enabled = true,
+        },
         filtered_items = {
           visible = true,
           hide_dotfiles = false,
