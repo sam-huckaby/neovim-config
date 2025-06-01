f2519c2212390dd20fd75e3a5a1289433089a7b4
Sam Huckaby <samhuckaby@gmail.com>
2025-05-31 21:10:11 -0400

    Update which-keys bindings to the new version

--- init.lua
+++ init.lua
@@ -277,15 +277,22 @@ require('lazy').setup({
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      local wk = require 'which-key'
+
+      wk.setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+      wk.add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>c_', hidden = true },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>d_', hidden = true },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>r_', hidden = true },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>s_', hidden = true },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>w_', hidden = true },
       }
     end,
   },
@@ -541,7 +548,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -551,7 +558,7 @@ require('lazy').setup({
         tsserver = {},
         ocamllsp = {},
         gopls = {},
-        graphql = {},
+        intelephense = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -810,6 +817,23 @@ require('lazy').setup({
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
+
+      local parsers = require 'nvim-treesitter.parsers'
+      ---@class ParserInfo[]
+      local parser_configs = parsers.get_parser_configs()
+
+      -- local revision = '01e5550cb60ef3532ace0c6df0480f6f406113ff'
+      local current = '335b2a44b4cdd9446f1c01434226267a61851405'
+
+      parser_configs.blade = {
+        install_info = {
+          url = 'https://github.com/EmranMR/tree-sitter-blade',
+          files = { 'src/parser.c' },
+          branch = 'main',
+          revision = current,
+        },
+        filetype = 'blade',
+      }
 
       -- There are additional nvim-treesitter modules that you can use to interact
       -- with nvim-treesitter. You should go explore a few and see what interests you:
